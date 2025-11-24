ea730bb0f6d36b15ee658dd7df4076fd89f7024b
Muhammed Ajasa <ajaxclopidia77@gmail.com>
2025-11-24 11:51:38 +0100

    Add deployment files

--- run.py
+++ run.py
@@ -1,11 +1,13 @@
 import uvicorn
+import os
 
 if __name__ == "__main__":
+    port = int(os.getenv("PORT", 8060))
     uvicorn.run(
         "app.main:app",
         host="0.0.0.0",
-        port=8060,
-        reload=True
+        port=port,
+        reload=False
     )
 
 
